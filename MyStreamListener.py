1a00bf26cf63a57ebe0ff9e0ddc023c4be4d3703
Ayodeji Marquis <ayodejimarquis@Ayodejis-MacBook-Pro.local>
2019-03-03 19:47:17 -0800

    final code for sentiment analysis

--- MyStreamListener.py
+++ MyStreamListener.py
@@ -1,11 +1,17 @@
 import tweepy
 import json
-import time
-
-#override tweepy.StreamListener to add logic to on_status
+import requests
 
 
 class MyStreamListener(tweepy.StreamListener):
+
+    # get this from Azure text analytic resource
+    sentiment_api_headers = {""}
+    sentiment_api_url = ""
+
+    # when you create a powerbi datastream, a post url is given. Insert that here
+    powerbi_url = ""
+
     totalCnt = 1
     buhari_cnt = 1
     atiku_cnt = 1
@@ -17,38 +23,135 @@ class MyStreamListener(tweepy.StreamListener):
     durotoye_list_of_text = []
     duke_list_of_text = []
 
+    totalApprovalCnt = 0
+    buhari_approval_cnt = 0
+    atiku_approval_cnt = 0
+    durotoye_approval_cnt = 0
+    duke_approval_cnt = 0
+
+    publish_cnt = 0
+    twitter_api = ""
+
     def on_status(self, status):
-        candidates = {}
-        document = {}
-        if self.totalCnt % 50 == 0:
-            time.sleep(60*5)
+
+        if self.totalCnt % 900 == 0:
+            self.process_sentiment_on_list()
+            self.publish_to_powerbi_and_twitter()
+            self.buhari_list_of_text = []
+            self.atiku_list_of_text = []
+            self.durotoye_list_of_text = []
+            self.duke_list_of_text = []
+
 
         if ('Buhari' or 'PMB' or 'APC') in str(status.text):
-            texts = {'id': self.buhari_cnt}
+            texts = {'language': 'en'}
+            texts['id'] = self.buhari_cnt
             self.buhari_cnt += 1
-            texts['language'] = 'en'
             texts['text'] = status.text
             self.buhari_list_of_text.append(texts)
 
         if ('Atiku' or 'PDP') in str(status.text):
-            texts = {'id': self.atiku_cnt}
+            texts = {'language': 'en'}
+            texts['id'] = self.atiku_cnt
             self.atiku_cnt += 1
-            texts['language'] = 'en'
             texts['text'] = status.text
             self.atiku_list_of_text.append(texts)
 
         if ('Durotoye' or 'Fela Durotoye' or 'ANN') in str(status.text):
-            texts = {'id': self.durotoye_cnt}
+            texts = {'language': 'en'}
+            texts['id'] = self.durotoye_cnt
             self.durotoye_cnt += 1
-            texts['language'] = 'en'
             texts['text'] = status.text
             self.durotoye_list_of_text.append(texts)
 
         if ('Donald Duke' or 'SDP') in str(status.text):
-            texts = {'id': self.duke_cnt}
+            texts = {'language': 'en'}
+            texts['id'] = self.duke_cnt
             self.duke_cnt += 1
-            texts['language'] = 'en'
             texts['text'] = status.text
             self.duke_list_of_text.append(texts)
         self.totalCnt += 1
-        print(status.text)
+
+    def process_sentiment_on_list(self):
+        if self.buhari_list_of_text:
+            document = {'documents': self.buhari_list_of_text}
+            scores = self.get_scores(document)
+            if scores is not None:
+                for i in range(len(scores)):
+                    score = scores[i]['score']
+                    if score > 0.74:
+                        self.buhari_approval_cnt += 1
+                        self.totalApprovalCnt += 1
+
+        if self.atiku_list_of_text:
+            document = {'documents': self.atiku_list_of_text}
+            scores = self.get_scores(document)
+            if scores is not None:
+                for i in range(len(scores)):
+                    score = scores[i]['score']
+                    if score > 0.74:
+                        self.atiku_approval_cnt += 1
+                        self.totalApprovalCnt += 1
+
+        if self.durotoye_list_of_text:
+            document = {'documents': self.durotoye_list_of_text}
+            scores = self.get_scores(document)
+            if scores is not None:
+                for i in range(len(scores)):
+                    score = scores[i]['score']
+                    if score > 0.74:
+                        self.durotoye_approval_cnt += 1
+                        self.totalApprovalCnt += 1
+
+        if self.duke_list_of_text:
+            document = {'documents': self.duke_list_of_text}
+            scores = self.get_scores(document)
+            if scores is not None:
+                for i in range(len(scores)):
+                    score = scores[i]['score']
+                    if score > 0.74:
+                        self.duke_approval_cnt += 1
+                        self.totalApprovalCnt += 1
+
+    def get_scores(self, document):
+        response = requests.post(self.sentiment_api_url, headers=self.sentiment_api_headers, json=document)
+        result = response.json()
+        scores = json.loads(json.dumps(result))
+        if 'documents' in scores:
+            return scores['documents']
+
+        return None
+
+    def publish_to_powerbi_and_twitter(self):
+        atiku_approval_rating = round((self.atiku_approval_cnt/self.totalApprovalCnt) * 100, 2)
+        durotoye_approval_rating = round((self.durotoye_approval_cnt/self.totalApprovalCnt) * 100, 2)
+        duke_approval_rating = round((self.duke_approval_cnt/self.totalApprovalCnt) * 100, 2)
+        buhari_approval_rating = round((self.buhari_approval_cnt/self.totalApprovalCnt) * 100, 2)
+
+        # dataa = [{'approvalRating': buhari_approval_rating, 'Candidate': 'Buhari'}]
+        # dataa = str(json.dumps(dataa))
+        # response = requests.post(self.powerbi_url, data=dataa)
+        # print(response)
+        #
+        # dataa = [{'approvalRating': atiku_approval_rating, 'Candidate': 'Atiku'}]
+        # dataa = str(json.dumps(dataa))
+        # response = requests.post(self.powerbi_url, data=dataa)
+        # print(response)
+        #
+        # dataa = [{'approvalRating': durotoye_approval_rating, 'Candidate': 'Fela Durotoye'}]
+        # dataa = str(json.dumps(dataa))
+        # response = requests.post(self.powerbi_url, data=dataa)
+        # print(response)
+        #
+        # dataa = [{'approvalRating': duke_approval_rating, 'Candidate': 'Duke'}]
+        # dataa = json.dumps(dataa)
+        # response = requests.post(self.powerbi_url, data=dataa)
+        # print(response)
+
+        tweet = 'id: ' + str(self.publish_cnt) + '\n' + 'Candidate: Buhari, approvalRating: ' + str(buhari_approval_rating) + \
+                '% \n' + 'Candidate: Atiku, approvalRating: ' + str(atiku_approval_rating) +'% \n' + \
+                'Candidate: Durotoye, approvalRating: ' + str(durotoye_approval_rating) +'% \n' + \
+                'Candidate: Duke, approvalRating: ' + str(duke_approval_rating) +'% \n'
+        self.twitter_api.update_status(tweet)
+        self.publish_cnt += 1
+        print('success!')
--- SentAnalysisPred.py
+++ SentAnalysisPred.py
@@ -1,25 +1,17 @@
 import tweepy
-import datetime
-import time
-import requests
 import MyStreamListener
-from datetime import datetime, timedelta
-
 
 class SentAnalysis:
 
     request_limit = 20
     api = ""
-    data = []
-    program_end_time = datetime.now() + timedelta(days=1)
-    headers = {"Ocp-Apim-Subscription-Key": "5abfc68054d94cf59d03d3194e231577"}
-    sentiment_api_url = "https://westus2.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
 
+    # insert your keys and secrets here
     twitter_keys = {
-        'consumer_key': "2ZDxwFRNlqbiBIdOtFu5Ilfql",
-        'consumer_secret': "kVruU1qKLAeRKuj49vOg5vFZKPOkYFP8WGQGtvedQFSyViK3pl",
-        'access_token_key': "389622860-wozPQKO4aEwetq1VaQnD06hx7kudH8mbPzKItsfT",
-        'access_token_secret': "bcTmQY6r2ap4iCWZkR4XmBnrObE6AG6mrXKz6627P8vzf"
+        'consumer_key': "",
+        'consumer_secret': "",
+        'access_token_key': "",
+        'access_token_secret': ""
     }
 
     def __init__(self, request_limit=20):
@@ -31,29 +23,17 @@ class SentAnalysis:
         auth.set_access_token(self.twitter_keys['access_token_key'], self.twitter_keys['access_token_secret'])
         self.api = tweepy.API(auth)
 
-    def livestream_of_tweets(self):
+    def get_tweet_and_perform_sent_analysis(self):
         myStreamListener = MyStreamListener.MyStreamListener()
+        myStreamListener.twitter_api = self.api
         myStream = tweepy.Stream(auth=self.api.auth, listener=myStreamListener)
         myStream.filter(track=['Buhari', 'PMB', 'APC', 'Atiku', 'PDP', 'Durotoye', 'Fela Durotoye', 'ANN', 'Donald Duke', 'SDP'],
                         languages=['en'])
-
-
-    def send_data_to_powerbi_api(self):
-
-            self.livestream_of_tweets()
-        # while datetime.now() < self.program_end_time:
-        #     time.sleep(60*60*5)
-        #     # get sentAnalysis data
-        #     documents = self.mine_user_tweets()
-        #     response = requests.post(self.sentiment_api_url, headers=self.headers, json=documents)
-        #     sentiments = response.json()
-        #     print(sentiments)
-
 
 
 
 
 
 if __name__ == '__main__':
-    mine = SentAnalysis()
-    print(mine.send_data_to_powerbi_api())
+        mine = SentAnalysis()
+        mine.get_tweet_and_perform_sent_analysis()
